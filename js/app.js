16689c6d656559c4d9b392b204bfe79451a68bba
yaninaslab <yana.belash@gmail.com>
2022-02-08 13:12:35 -0500

    Added modal and other basic stuff

--- js/app.js
+++ js/app.js
@@ -17,3 +17,22 @@ form.addEventListener("submit", (e) => {
     list.removeChild(item);
   });
 });
+
+const modal = document.querySelector("#modal");
+const openModalButton = document.querySelector("#open-modal-btn");
+const closeModalButton = document.querySelector("#close-modal-btn");
+const overlay = document.querySelector("#overlay");
+
+openModalButton.addEventListener("click", () => {
+  modal.classList.add("open");
+  overlay.classList.add("open");
+});
+
+closeModalButton.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+function closeModal() {
+  modal.classList.remove("open");
+  overlay.classList.remove("open");
+}
